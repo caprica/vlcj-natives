efe6c66973a4b77ac29bc3295f6f326fe9215a14
Mark Lee <mark.lee@capricasoftware.co.uk>
2019-03-22 22:01:48 +0000

    Remove obsolete libvlc_new_with_builtins

--- src/main/java/uk/co/caprica/vlcj/binding/LibVlc.java
+++ src/main/java/uk/co/caprica/vlcj/binding/LibVlc.java
@@ -138,25 +138,6 @@ public interface LibVlc extends Library {
     libvlc_instance_t libvlc_new(int argc, String[] argv);
 
     /**
-     * Create and initialize a libvlc instance.
-     *
-     * @param argc the number of arguments
-     * @param argv command-line-type arguments
-     * @param builtins a NULL terminated array of @see vlc_plugin.
-     * @return the libvlc instance or NULL in case of error
-     *
-     * <pre>
-     * {
-     *     vlc_declare_plugin(mp4);
-     *     vlc_declare_plugin(dummy);
-     *     const void **builtins = { vlc_plugin(mp4), vlc_plugin(dummy), NULL };
-     *     libvlc_instance_t *vlc = libvlc_new_with_builtins(argc, argv, builtins);
-     * }
-     * </pre>
-     */
-    libvlc_instance_t libvlc_new_with_builtins(int argc, String[] argv, Pointer[] builtins);
-
-    /**
      * Decrement the reference count of a libvlc instance, and destroy it if it reaches zero.
      *
      * @param p_instance the instance to destroy
