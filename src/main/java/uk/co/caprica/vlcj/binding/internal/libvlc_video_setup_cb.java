0d6da34c0f289a6364cb8302ca55e5af19f103da
Mark Lee <mark.lee@capricasoftware.co.uk>
2019-03-27 17:16:28 +0000

    Add callback classes for the native video callbacks
    This is new API that will not be available until VLC 4.0.0.

---
+++ src/main/java/uk/co/caprica/vlcj/binding/internal/libvlc_video_setup_cb.java
@@ -0,0 +1,40 @@
+/*
+ * This file is part of VLCJ.
+ *
+ * VLCJ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VLCJ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VLCJ.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2009-2019 Caprica Software Limited.
+ */
+
+package uk.co.caprica.vlcj.binding.internal;
+
+import com.sun.jna.Callback;
+import com.sun.jna.Pointer;
+
+/**
+ *
+ */
+public interface libvlc_video_setup_cb extends Callback {
+
+    /**
+     * Callback prototype called to initialize user data.
+     *
+     * @param opaque private pointer passed to the @a libvlc_video_set_output_callbacks() [IN]
+     * @return true on success
+     *
+     * @since LibVLC 4.0.0 or later
+     */
+    int setup(Pointer opaque);
+
+}
