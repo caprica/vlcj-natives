aa0d6516064707462d4d43f622b3811b98ed58e4
Mark Lee <mark.lee@capricasoftware.co.uk>
2022-12-16 19:50:12 +0000

    Native audio mixmode/stereomode/channel changes

---
+++ src/main/java/uk/co/caprica/vlcj/binding/internal/libvlc_audio_output_stereomode_t.java
@@ -0,0 +1,50 @@
+/*
+ * This file is part of VLCJ.
+ *
+ * VLCJ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VLCJ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VLCJ.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2009-2021 Caprica Software Limited.
+ */
+
+package uk.co.caprica.vlcj.binding.internal;
+
+import com.sun.jna.Pointer;
+import com.sun.jna.Structure;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ *
+ */
+public enum libvlc_audio_output_stereomode_t {
+
+    libvlc_AudioStereoMode_Error(-1),
+    libvlc_AudioStereoMode_Stereo(1),
+    libvlc_AudioStereoMode_RStereo(2),
+    libvlc_AudioStereoMode_Left(3),
+    libvlc_AudioStereoMode_Right(4),
+    libvlc_AudioStereoMode_Dolbys(5);
+
+    private final int intValue;
+
+    libvlc_audio_output_stereomode_t(int intValue) {
+        this.intValue = intValue;
+    }
+
+    public int intValue() {
+        return intValue;
+    }
+}
